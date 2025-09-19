d806d94c1aa34d6b8bbf423ea237cce3aaeb4ed8
Redmond01 <okeraymondjesutofunmi99@gmail.com>
2025-09-19 11:31:02 +0200

    update

--- app/test.ts
+++ app/test.ts
@@ -1,15 +1,36 @@
-import {ApolloServer,} from '@apollo/server'
+import { ApolloServer, } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone'
+const data = [{ name: 'raymond', age: 26 },{ name: 'motunrayo', age: 12 }]
 
-const test = `
-    type Name={
-    name:'raymond'
+const test = `#graphql
+    type Name {
+        name:String
+        age:Int
+    }
+
+    type Query{
+        name:Name
+        age:[Name]
     }
 `
 
-const graphQuery = `
-    type Query={
-       name: ${test}
+const resolver = {
+    Query: {
+        name: () => {
+          return data[0]
+        },
+        age:() => {
+          return data
+        }
     }
+}
 
-`
+const server = new ApolloServer({
+    typeDefs: test, resolvers: resolver
+})
 
+
+const { url } = await startStandaloneServer(server);
+
+
+console.log(`connection is on at ${url}`)
